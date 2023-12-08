e95fa565c4dbfd15d5171e6066e8334d6357dd47
ErvinasVlks <e.vilkaitis@student.utwente.nl>
2023-12-08 10:27:44 +0100

    Expanded some class with attributes/methods

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Hand {
+
+
+    private ArrayList<Card> hand;
+    private Player player;
+
+
+    public Hand(ArrayList<Card> cards, Player player){
+        this.hand = cards;
+        this.player = player;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,11 @@ public class Player {
 
 
     private String name;
+    private Hand hand;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public Card draw(){return null;}
 }
